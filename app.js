b93fa93e07a9df3c2edfb04dc77151ce2e560251
Damola <eoajayidamola@outlook.com>
2021-04-17 09:13:01 +0100

    My first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const http= require('http')
+const path= require('path')
+const  fs = require('fs')
+const fetch = require('node-fetch');
+
+
+
+//create server
+
+const server = http.createServer (function(req,res){
+    fetch('http://jsonplaceholder.typicode.com/posts', {
+        body: JSON.stringify(),
+      header: { 'Content-Type': ('application/json' )}
+    })
+    .then(res => res.json())
+      .then(json => console.log(json))
+      .catch(err => console.log(err))
+})
+
+// createport
+const port=3000
+
+server.listen(3000,'localhost');
+
+console.log("server is listenning on port 3000")
+
+
+
+
+
